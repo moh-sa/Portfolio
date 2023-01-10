868665aed4c2aa315d0259dbe4899d7520c06ea4
moh-sa <tno.rappelz@gmail.com>
2023-01-10 16:37:02 +0300

    removed the color from the header

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,7 +3,7 @@ import Card from "./common/Card";
 const Projects = ({ projects }: { projects: CardProps[] }) => {
   return (
     <>
-      <h1 className="mb-4 text-3xl text-[#d2bfb7]">Projects</h1>
+      <h1 className="mb-4 text-3xl">Projects</h1>
       <div className="grid grid-cols-1 justify-items-center gap-8 lg:grid-cols-2 mx-2">
         {projects.map((project) => (
           <Card key={project.id} {...project} />
