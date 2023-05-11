ea1e5e4d0f59ecc64dfe84e34c430f45a8f50cea
moh-sa <tno.rappelz@gmail.com>
2023-05-11 14:38:25 +0300

    added more metadata infos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,40 @@ import styles from "./layout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "Mohammed Sobhi Portfolio";
+const description =
+  "A Computer Engineer obsessed with crafting robust, user-friendly, and easy to access web applications";
+
 export const metadata = {
-  title: "Mohammed Sobhi Portfolio",
-  description:
-    "A Computer Engineer obsessed with crafting robust, user-friendly, and easy to access web applications.",
+  title: title,
+  description: description,
+  keywords: [
+    "full-stack",
+    "web developer",
+    "react.js",
+    "next.js",
+    "mongoDB",
+    "postgreSQL",
+    "tailwind",
+    "CSS",
+    "native css",
+    "typescript",
+    "portfolio",
+    "mohammed sobhi",
+    "Tno",
+    "MSA",
+    "Tno MSA",
+  ],
+  openGraph: {
+    title: title,
+    description: description,
+    url: "https://moh-sa.dev",
+  },
+  twitter: {
+    title: title,
+    description: description,
+    creator: "@Tno_MSA",
+  },
 };
 
 export default function RootLayout({
