2d7a5e274e38de1b50611fe0298f4996ab16186a
moh-sa <tno.rappelz@gmail.com>
2023-12-15 22:57:01 +0200

    changed the header to h2, and removed the <a> from it

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import styles from "./Card.module.css";
-import Button from "../button/Button";
-import Flag from "../Flag/Flag";
-import Tag from "../Tag/Tag";
-import { ProjectType } from "$/data/projects";
+import Image from 'next/image';
+import styles from './Card.module.css';
+import Link from '../link/Link';
+import Flag from '../Flag/Flag';
+import Tag from '../Tag/Tag';
+import { ProjectType } from '$/data/projects';
 
 const Card = ({ ...props }: ProjectType) => {
   return (
@@ -14,20 +14,16 @@ const Card = ({ ...props }: ProjectType) => {
           className={styles.image}
           src={props.imgSrc}
           alt={props.imgAlt}
-          style={{ width: "100%", height: "100%" }}
-          placeholder="blur"
-          loading="lazy"
+          style={{ width: '100%', height: '100%' }}
+          placeholder='blur'
+          loading='lazy'
         />
       </div>
 
       {/* 🌟 Title + 🗓️ Date + 🏷️ Original */}
       <header className={styles.headerWrapper}>
-        <h1>
-          <a href={props.demoSrc} target="_blank" tabIndex={-1}>
-            {props.title}
-          </a>
-        </h1>
-        <h2>{props.year}</h2>
+        <h2>{props.title}</h2>
+        <time dateTime={props.year.toString()}>{props.year}</time>
         {props.isOriginal && <Flag />}
       </header>
 
@@ -36,7 +32,10 @@ const Card = ({ ...props }: ProjectType) => {
         <p className={styles.description}>{props.description}</p>
         <ul className={styles.techs}>
           {props.tech.map((tech, index) => (
-            <Tag key={index} text={tech} />
+            <Tag
+              key={index}
+              text={tech}
+            />
           ))}
         </ul>
       </section>
@@ -44,14 +43,25 @@ const Card = ({ ...props }: ProjectType) => {
       {/* 🔗 Links to github + live demo */}
       <footer className={styles.footerWrapper}>
         {props.demoSrc && (
-          <Button icon="demo" href={props.demoSrc} text="Live demo" />
+          <Link
+            icon='demo'
+            href={props.demoSrc}
+            text='Live demo'
+          />
         )}
         {props.repoSrc && (
-          <Button icon="github" href={props.repoSrc} text="Code" />
+          <Link
+            icon='github'
+            href={props.repoSrc}
+            text='Code'
+          />
         )}
 
         {!props.demoSrc && !props.repoSrc && (
-          <Button as="span" icon="notAllowed" text="Not Available" />
+          <Link
+            icon='notAllowed'
+            text='Not Available'
+          />
         )}
       </footer>
     </article>
