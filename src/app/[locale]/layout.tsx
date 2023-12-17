af58fef61bd30857e6efde5367e74f788d54bc09
moh-sa <tno.rappelz@gmail.com>
2023-12-17 18:44:23 +0200

    added "generateStaticParams" and a comment

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,7 +2,10 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import { Open_Sans, Cairo } from 'next/font/google';
 import styles from './layout.module.css';
-import { unstable_setRequestLocale } from 'next-intl/server';
+import {
+  getTranslations,
+  unstable_setRequestLocale,
+} from 'next-intl/server';
 import { redirect } from 'next/navigation';
 
 const roboto = Open_Sans({
@@ -15,54 +18,52 @@ const cairo = Cairo({
   subsets: ['arabic', 'latin'],
 });
 
-// export async function generateMetadata({params: {locale}}) {
-//   const t = await getTranslations({locale, namespace: 'Metadata'});
-
-//   return {
-//     title: t('title')
-//   };
-// }
-
-const title = 'Mohammed Sobhi Portfolio';
-const description =
-  'A Computer Engineer obsessed with crafting robust, user-friendly, and easy to access web applications';
-
-export const metadata = {
-  title: title,
-  description: description,
-  keywords: [
-    'full-stack',
-    'web developer',
-    'react.js',
-    'next.js',
-    'mongoDB',
-    'postgreSQL',
-    'tailwind',
-    'CSS',
-    'native css',
-    'typescript',
-    'portfolio',
-    'mohammed sobhi',
-    'Tno',
-    'MSA',
-    'Tno MSA',
-  ],
-  openGraph: {
-    title: title,
-    description: description,
-    url: 'https://moh-sa.dev',
-  },
-  twitter: {
-    title: title,
-    description: description,
-    creator: '@Tno_MSA',
-  },
-};
-
 export const locales = ['en', 'ar'];
 
+// Due to the 'next-intl' current's version limitation
+//This function is needed to create static pages
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
+}
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: 'en' | 'ar' };
+}) {
+  const t = await getTranslations({ locale, namespace: 'Metadata' });
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    keywords: [
+      'full-stack',
+      'web developer',
+      'react.js',
+      'next.js',
+      'mongoDB',
+      'postgreSQL',
+      'tailwind',
+      'CSS',
+      'native css',
+      'typescript',
+      'portfolio',
+      'mohammed sobhi',
+      'Tno',
+      'MSA',
+      'Tno MSA',
+    ],
+    openGraph: {
+      title: t('title'),
+      description: t('description'),
+      url: 'https://moh-sa.dev',
+    },
+    twitter: {
+      title: t('title'),
+      description: t('description'),
+      creator: '@Tno_MSA',
+    },
+  };
 }
 
 export default async function RootLayout({
@@ -74,6 +75,8 @@ export default async function RootLayout({
 }) {
   if (!locales.includes(locale as any)) redirect('/en');
 
+  // Due to the 'next-intl' current's version limitation
+  //This line is needed to create static pages
   unstable_setRequestLocale(locale);
 
   const fontFamily =
