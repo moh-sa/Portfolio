baafe645d1e71c62b21184965e4397d4ac3037b1
moh-sa <tno.rappelz@gmail.com>
2023-12-17 21:06:10 +0200

    added "text" and "aria" props and their types + replaced the svg with string

--- src/components/toggleLocale/ToggleLocale.tsx
+++ src/components/toggleLocale/ToggleLocale.tsx
@@ -2,15 +2,15 @@
 import { useRouter, usePathname } from 'next/navigation';
 import styles from './ToggleLocale.module.css';
 
-export default function Test() {
+type ToggleLocaleProps = {
+  text: string;
+  aria: string;
+};
+
+export default function Test({ text, aria }: ToggleLocaleProps) {
   const router = useRouter();
   const pathname = usePathname();
   const lang = pathname.replace('/', '');
-
-  const ariaLabel =
-    lang === 'ar'
-      ? 'تغيير اللغة إلى الانجليزية'
-      : 'Switch to Arabic language';
 
   function languageToggleHandler() {
     if (lang === 'ar') router.push('/en');
@@ -21,9 +21,9 @@ export default function Test() {
     <button
       className={styles.button}
       onClick={languageToggleHandler}
-      aria-label={ariaLabel}
+      aria-label={aria}
     >
-      <svg
+      {/* <svg
         xmlns='http://www.w3.org/2000/svg'
         fill='none'
         viewBox='0 0 24 24'
@@ -36,7 +36,8 @@ export default function Test() {
           strokeLinejoin='round'
           d='M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802'
         />
-      </svg>
+      </svg> */}
+      {text}
     </button>
   );
 }
