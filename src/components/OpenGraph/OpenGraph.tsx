b10bb33996e9b957f6c0ecaaacde4f1d170f6020
moh-sa <tno.rappelz@gmail.com>
2023-12-17 22:13:26 +0200

    moved to the [locale] folder

--- src/components/OpenGraph/OpenGraph.tsx
+++ src/components/OpenGraph/OpenGraph.tsx
@@ -2,25 +2,25 @@ const OpenGraph = () => {
   return (
     <div
       style={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        flexWrap: "nowrap",
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
         backgroundImage:
-          "radial-gradient(circle, rgba(27,32,54,1) 0%, rgba(35,41,70,1) 75%)",
-        color: "#b8c1ec",
-        gap: "-64px",
+          'radial-gradient(circle, rgba(27,32,54,1) 0%, rgba(35,41,70,1) 75%)',
+        color: '#b8c1ec',
+        gap: '-64px',
       }}
     >
       <h1
         style={{
-          fontSize: "70px",
+          fontSize: '70px',
           fontWeight: 600,
           lineHeight: 1.8,
-          color: "#e9e1dd",
+          color: '#e9e1dd',
         }}
       >
         Mohammed Sobhi
@@ -28,7 +28,7 @@ const OpenGraph = () => {
 
       <h2
         style={{
-          fontSize: "55px",
+          fontSize: '55px',
           fontWeight: 500,
         }}
       >
