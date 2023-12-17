c401c3e25a12682f46b77a75d321e366dee173d7
moh-sa <tno.rappelz@gmail.com>
2023-12-17 18:45:00 +0200

    added hardcoded type to locale and a comment

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,19 +1,18 @@
 import AboutMe from '$/components/aboutMe/AboutMe';
 import Projects from '$/components/projects/Projects';
-import { useTranslations } from 'next-intl';
 import { unstable_setRequestLocale } from 'next-intl/server';
 
 export default function Home({
   params: { locale },
 }: {
-  params: { locale: string };
+  params: { locale: 'ar' | 'en' };
 }) {
+  // Due to the 'next-intl' current's version limitation
+  //This line is needed to create static pages
   unstable_setRequestLocale(locale);
 
-  const t = useTranslations('Index');
   return (
     <>
-      {/* <h1>{t('title')}</h1> */}
       <AboutMe />
       <Projects />
     </>
