b6a5169605e6affa4eae5974b933bcffca40e8c7
moh-sa <tno.rappelz@gmail.com>
2024-05-09 20:28:30 +0300

    added basic metadata, and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,29 @@
-import { ReactNode } from 'react';
+import type { Metadata, Viewport } from "next";
+import { keywords } from "~/config/metadata";
+import { env } from "~/env";
+import "~/styles/globals.css";
 
-type Props = {
-  children: ReactNode;
-};
-
-// Since we have a `not-found.tsx` page on the root, a layout file
-// is required, even if it's just passing children through.
-export default function RootLayout({ children }: Props) {
+// Since we have a "not-found.tsx" page, a layout is required.
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return children;
 }
+
+export const viewport: Viewport = {
+  themeColor: "#242447",
+  colorScheme: "dark",
+};
+
+export const metadata: Metadata = {
+  keywords: [...keywords],
+  alternates: {
+    canonical: `${env.NEXT_PUBLIC_URL}`,
+    languages: {
+      en: `${env.NEXT_PUBLIC_URL}/en`,
+      ar: `${env.NEXT_PUBLIC_URL}/ar`,
+    },
+  },
+};
