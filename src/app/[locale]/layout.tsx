e48672364d0e730bcb15110c71e5c0bc14bc0c24
moh-sa <tno.rappelz@gmail.com>
2024-05-09 20:36:05 +0300

    Changed in preparation for v3

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,108 +1,54 @@
-import './globals.css';
-import styles from './layout.module.css';
-import { Analytics } from '@vercel/analytics/react';
-import { Open_Sans, Cairo } from 'next/font/google';
-import {
-  getTranslations,
-  unstable_setRequestLocale,
-} from 'next-intl/server';
-import { notFound } from 'next/navigation';
-import { ReactNode } from 'react';
-import { Metadata } from 'next';
+import { Analytics } from "@vercel/analytics/next";
+import type { Metadata } from "next";
+import { Cairo, Inter } from "next/font/google";
+import { metaInfo } from "~/config/metadata";
+import { Locales } from "~/types";
+import { getOpenGraphData } from "~/utils/metadata";
 
-const roboto = Open_Sans({
-  weight: ['300', '400', '500', '600', '700', '800'],
-  subsets: ['latin'],
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 const cairo = Cairo({
-  weight: ['300', '400', '500', '600', '700', '800'],
-  subsets: ['arabic', 'latin'],
+  subsets: ["arabic", "latin"],
+  variable: "--font-sans",
 });
 
-type Props = {
-  children: ReactNode;
-  params: { locale: string };
-};
-
-export const locales = ['en', 'ar'];
-
-// Due to the 'next-intl' current's version limitation
-// this line is needed to enable static rendering
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
-
-export async function generateMetadata({
-  params: { locale },
-}: Omit<Props, 'children'>): Promise<Metadata> {
-  const t = await getTranslations({ locale, namespace: 'Metadata' });
-
-  return {
-    title: t('title'),
-    description: t('description'),
-    keywords: [
-      'full-stack',
-      'web developer',
-      'react.js',
-      'next.js',
-      'mongoDB',
-      'postgreSQL',
-      'tailwind',
-      'CSS',
-      'native css',
-      'typescript',
-      'portfolio',
-      'mohammed sobhi',
-      'Tno',
-      'MSA',
-      'Tno MSA',
-    ],
-    openGraph: {
-      title: t('title'),
-      description: t('description'),
-      url: 'https://moh-sa.dev',
-      type: 'website',
-    },
-    twitter: {
-      title: t('title'),
-      description: t('description'),
-      creator: '@Tno_MSA',
-      card: 'summary_large_image',
-    },
+// TODO: check if needed
+// export async function generateStaticParams() {
+//   return [{ locale: "en" }, { locale: "ar" }];
+// }
+type params = {
+  children: React.ReactNode;
+  params: {
+    locale: Locales;
   };
-}
-
-export default async function LocaleLayout({
-  children,
-  params: { locale },
-}: Props) {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
-
-  // Due to the 'next-intl' current's version limitation
-  // This line is needed to enable static rendering
-  unstable_setRequestLocale(locale);
-
-  const dir = locale === 'ar' ? 'rtl' : 'ltr';
-
-  const fontFamily =
-    locale === 'ar' ? cairo.className : roboto.className;
-
+};
+export default function RootLayout({ children, params }: params) {
+  const localeConfig = {
+    locale: params.locale === Locales.ENGLISH ? "en" : "ar",
+    dir: params.locale === Locales.ENGLISH ? "ltr" : "rtl",
+    fontFamily:
+      params.locale === Locales.ENGLISH ? inter.variable : cairo.variable,
+  };
   return (
-    <html lang={locale}>
-      <body
-        style={{ background: 'hsl(230 33% 26%)' }}
-        className={`${fontFamily}`}
-      >
-        <main
-          className={styles.main}
-          dir={dir}
-        >
-          {children}
-        </main>
+    <html lang={localeConfig.locale} dir={localeConfig.dir}>
+      <body className={`font-sans ${localeConfig.fontFamily}`}>
+        {children}
         <Analytics />
       </body>
     </html>
   );
+}
+
+export function generateMetadata({ params }: Pick<params, "params">): Metadata {
+  const locale = params.locale === Locales.ENGLISH ? "en" : "ar";
+  const { og, tw } = getOpenGraphData({ locale: locale });
+  return {
+    title: metaInfo[locale].title,
+    description: metaInfo[locale].description,
+    openGraph: { ...og },
+    twitter: { ...tw },
+  };
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,20 +1,46 @@
-import AboutMe from '$/components/aboutMe/AboutMe';
-import Projects from '$/components/projects/Projects';
-import { unstable_setRequestLocale } from 'next-intl/server';
+import Link from "next/link";
+import { getLocaleFile } from "~/locales/locales";
+import { Locales } from "~/types";
 
-export default function Home({
-  params: { locale },
-}: {
-  params: { locale: 'ar' | 'en' };
-}) {
-  // Due to the 'next-intl' current's version limitation
-  //This line is needed to create static pages
-  unstable_setRequestLocale(locale);
+type params = {
+  params: {
+    locale: Locales;
+  };
+};
+
+export default async function HomePage({ params }: params) {
+  const loc = await getLocaleFile(params.locale);
+  const arrow = params.locale === Locales.ENGLISH ? "→" : "←";
 
   return (
-    <>
-      <AboutMe />
-      <Projects />
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          {loc.home.title}
+        </h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="https://create.t3.gg/en/usage/first-steps"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">
+              {loc.home.firstSteps.title} {arrow}
+            </h3>
+            <div className="text-lg">{loc.home.firstSteps.description}</div>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="https://create.t3.gg/en/introduction"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">
+              {loc.home.documentation.title} {arrow}
+            </h3>
+            <div className="text-lg">{loc.home.documentation.description}</div>
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
