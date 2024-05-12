1a6c73ae411e72646a6d68f934197cfab6537dfc
moh-sa <tno.rappelz@gmail.com>
2024-05-12 17:41:49 +0300

    fixed the argument's type + used react.cache

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,10 +1,11 @@
 import { revalidatePath } from "next/cache";
+import { cache } from "react";
 import "server-only";
 import { ZodError } from "zod";
 import { db } from "./db";
 import { projectSchema, projects, techSchema, techs } from "./db/schema";
 
-export async function getAllProjects() {
+export const getAllProjects = cache(async () => {
   try {
     return await db.query.projects.findMany({
       with: {
@@ -21,50 +22,54 @@ export async function getAllProjects() {
       cause: error,
     });
   }
-}
+});
 
-export async function createNewProject(project: typeof projectSchema) {
-  try {
-    const newProject = projectSchema.insert.parse(project);
+export const createNewProject = cache(
+  async (project: typeof projectSchema.insert._type) => {
+    try {
+      const newProject = projectSchema.insert.parse(project);
 
-    const [newProjectID] = await db
-      .insert(projects)
-      .values({ ...newProject })
-      .returning({ id: projects.id });
+      const [newProjectID] = await db
+        .insert(projects)
+        .values({ ...newProject })
+        .returning({ id: projects.id });
 
-    revalidatePath("/[locale]", "page");
+      revalidatePath("/[locale]", "page");
 
-    return newProjectID;
-  } catch (error) {
-    console.log(error);
-    if (error instanceof ZodError) {
-      throw new Error(error.message, { cause: error.cause });
-    } else {
-      throw new Error("Something went wrong while creating a new project", {
-        cause: error,
-      });
+      return newProjectID;
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        throw new Error(error.message, { cause: error.cause });
+      } else {
+        throw new Error("Something went wrong while creating a new project", {
+          cause: error,
+        });
+      }
     }
-  }
-}
+  },
+);
 
-export async function createNewTech(tech: typeof techSchema) {
-  try {
-    const newTech = techSchema.insert.parse(tech);
+export const createNewTech = cache(
+  async (tech: typeof techSchema.insert._type) => {
+    try {
+      const newTech = techSchema.insert.parse(tech);
 
-    const [newTechID] = await db
-      .insert(techs)
-      .values({ ...newTech })
-      .returning({ id: techs.id });
+      const [newTechID] = await db
+        .insert(techs)
+        .values({ ...newTech })
+        .returning({ id: techs.id });
 
-    return newTechID;
-  } catch (error) {
-    console.log(error);
-    if (error instanceof ZodError) {
-      throw new Error(error.message, { cause: error.cause });
-    } else {
-      throw new Error("Something went wrong while creating a new tech", {
-        cause: error,
-      });
+      return newTechID;
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        throw new Error(error.message, { cause: error.cause });
+      } else {
+        throw new Error("Something went wrong while creating a new tech", {
+          cause: error,
+        });
+      }
     }
-  }
-}
+  },
+);
