333ffe0deba60d02164a58a36ec4856212db13f5
moh-sa <tno.rappelz@gmail.com>
2024-05-21 00:26:04 +0300

    v2 old files

--- src/components/Flag/Flag.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Flag.module.css";
-
-const Flag = ({ text = "Original" }: { text?: string }) => {
-  return <span className={styles.flag}>{text}</span>;
-};
-
-export default Flag;
--- src/components/Tag/Tag.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Tag.module.css";
-
-const Tag = ({ text }: { text: string }) => {
-  return <li className={styles.tag}>{text}</li>;
-};
-
-export default Tag;
