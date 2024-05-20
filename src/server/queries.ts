6572b92f53a31670161fe67b4c20688772fd6569
moh-sa <tno.rappelz@gmail.com>
2024-05-20 23:57:46 +0300

    get All Techs, create New Project Tech (M-t-M table), delete a project, and delete a tech

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,9 +1,27 @@
+import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { cache } from "react";
 import "server-only";
 import { ZodError } from "zod";
 import { db } from "./db";
-import { projectSchema, projects, techSchema, techs } from "./db/schema";
+import {
+  projectTechs,
+  projectZodSchema,
+  projects,
+  techZodSchema,
+  techs,
+} from "./db/schema";
+
+export const getAllTechs = cache(async () => {
+  try {
+    return await db.query.projects.findMany();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Something went wrong while getting all Techs", {
+      cause: error,
+    });
+  }
+});
 
 export const getAllProjects = cache(async () => {
   try {
@@ -25,9 +43,9 @@ export const getAllProjects = cache(async () => {
 });
 
 export const createNewProject = cache(
-  async (project: typeof projectSchema.insert._type) => {
+  async (project: typeof projectZodSchema.insert._type) => {
     try {
-      const newProject = projectSchema.insert.parse(project);
+      const newProject = projectZodSchema.insert.parse(project);
 
       const [newProjectID] = await db
         .insert(projects)
@@ -51,9 +69,9 @@ export const createNewProject = cache(
 );
 
 export const createNewTech = cache(
-  async (tech: typeof techSchema.insert._type) => {
+  async (tech: typeof techZodSchema.insert._type) => {
     try {
-      const newTech = techSchema.insert.parse(tech);
+      const newTech = techZodSchema.insert.parse(tech);
 
       const [newTechID] = await db
         .insert(techs)
@@ -73,3 +91,53 @@ export const createNewTech = cache(
     }
   },
 );
+
+export const createNewProjectTech = cache(
+  async (projectID: number, techID: number) => {
+    try {
+      const [newProjectTechID] = await db
+        .insert(projectTechs)
+        .values({ projectID, techID });
+
+      return newProjectTechID;
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        throw new Error(error.message, { cause: error.cause });
+      } else {
+        throw new Error(
+          "Something went wrong while creating a new project tech",
+          {
+            cause: error,
+          },
+        );
+      }
+    }
+  },
+);
+
+export const deleteProject = cache(async (projectID: number) => {
+  try {
+    await db.delete(projectTechs).where(eq(projectTechs.projectID, projectID));
+    await db.delete(projects).where(eq(projects.id, projectID));
+    return { message: "Project deleted successfully" };
+  } catch (error) {
+    console.log(error);
+    throw new Error("Something went wrong while getting all projects", {
+      cause: error,
+    });
+  }
+});
+
+export const deleteTech = cache(async (techID: number) => {
+  try {
+    await db.delete(projectTechs).where(eq(projectTechs.techID, techID));
+    await db.delete(techs).where(eq(techs.id, techID));
+    return { message: "Tech deleted successfully" };
+  } catch (error) {
+    console.log(error);
+    throw new Error("Something went wrong while getting all projects", {
+      cause: error,
+    });
+  }
+});
