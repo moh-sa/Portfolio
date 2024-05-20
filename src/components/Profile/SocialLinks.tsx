cd750893cad348b90ad6c204f7f78c0c3a81ad67
moh-sa <tno.rappelz@gmail.com>
2024-05-21 00:11:39 +0300

    a wrapper to social links in profile

---
+++ src/components/Profile/SocialLinks.tsx
@@ -0,0 +1,20 @@
+import { Icon, LinkButton } from "~/components";
+import { type TLocales } from "~/locales/locales";
+
+type TProps = {
+  data: TLocales["profile"]["contact"]["links"];
+};
+export async function SocialLinks({ data }: TProps) {
+  return (
+    <ul className="space-y-2">
+      {data.map((link, index) => (
+        <li key={`${index}-${link.label}`}>
+          <LinkButton profile url={link.url}>
+            <Icon icon={link.icon} />
+            {link.label}
+          </LinkButton>
+        </li>
+      ))}
+    </ul>
+  );
+}
