ff77a244f34e15c12679738113aca571b3a27ea1
moh-sa <tno.rappelz@gmail.com>
2024-05-21 00:24:39 +0300

    hopefully this is final version

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
+import { ProfileSection, ProjectSection } from "~/components";
 import { getLocaleFile } from "~/locales/locales";
-import { Locales } from "~/types";
-
+import { type Locales } from "~/types";
 export async function generateStaticParams() {
   return [{ locale: "en" }, { locale: "ar" }];
 }
@@ -12,39 +11,16 @@ type params = {
   };
 };
 
-export default async function LocalePage({ params }: params) {
-  const loc = await getLocaleFile(params.locale);
-  const arrow = params.locale === Locales.ENGLISH ? "→" : "←";
+export default async function LocalePage({
+  params: { locale: localeType },
+}: params) {
+  const loc = await getLocaleFile(localeType);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          {loc.home.title}
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">
-              {loc.home.firstSteps.title} {arrow}
-            </h3>
-            <div className="text-lg">{loc.home.firstSteps.description}</div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">
-              {loc.home.documentation.title} {arrow}
-            </h3>
-            <div className="text-lg">{loc.home.documentation.description}</div>
-          </Link>
-        </div>
-      </div>
+    <main className="container mx-auto flex flex-col md:my-4 md:rounded-md md:outline md:outline-4 md:outline-obsidian-500 md:drop-shadow-md lg:my-8 lg:flex lg:min-h-[calc(100dvh-64px)] lg:flex-row">
+      <ProfileSection localeType={localeType} localeData={loc.profile} />
+
+      <ProjectSection localeType={localeType} localeData={loc.projects} />
     </main>
   );
 }
