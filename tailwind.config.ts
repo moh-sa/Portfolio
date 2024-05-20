a9802fbd9d1baa9a92273d8610c167d6c2b2b447
moh-sa <tno.rappelz@gmail.com>
2024-05-20 18:02:40 +0300

    added a list of new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,22 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        navy: {
+          light: "hsl(230 33% 82%)",
+          DEFAULT: "hsl(240, 33%, 28%)",
+          dark: "hsl(240, 33%, 26%)",
+          darker: "hsl(240, 33%, 16%)",
+        },
+        charcoal: {
+          800: "hsl(240 33% 20%)",
+          900: "hsl(240, 33%, 12%)",
+          950: "hsl(240, 33%, 8%)",
+        },
+        indigo: {
+          1000: "hsl(240, 23%, 36%)",
+        },
+      },
     },
   },
   plugins: [],
