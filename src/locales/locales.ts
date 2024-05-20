fd5e2bb26f6442069ccbfa9ce28feca475c477ac
moh-sa <tno.rappelz@gmail.com>
2024-05-21 00:07:06 +0300

    changed to the new locale files, cached the call, exported the types

--- src/locales/locales.ts
+++ src/locales/locales.ts
@@ -1,10 +1,15 @@
+import { cache } from "react";
 import "server-only";
 import type { Locales } from "~/types";
 
 // dynamic import of the locale file.
 const locales = {
-  en: () => import("~/locales/en.json").then((module) => module.default),
-  ar: () => import("~/locales/ar.json").then((module) => module.default),
+  en: () => import("~/locales/en").then((module) => module.default),
+  ar: () => import("~/locales/ar").then((module) => module.default),
 };
 
-export const getLocaleFile = async (locale: Locales) => locales[locale]();
+export const getLocaleFile = cache(async (locale: Locales) =>
+  locales[locale](),
+);
+
+export type TLocales = Awaited<ReturnType<typeof getLocaleFile>>;
