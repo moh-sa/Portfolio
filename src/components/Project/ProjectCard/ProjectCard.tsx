d43660e50a9c7d890ba34212308cb35de89c00cd
moh-sa <tno.rappelz@gmail.com>
2024-05-26 22:03:13 +0300

    changed the card style

--- src/components/Project/ProjectCard/ProjectCard.tsx
+++ src/components/Project/ProjectCard/ProjectCard.tsx
@@ -28,15 +28,19 @@ export function ProjectCard({
   localeData,
 }: TProps) {
   return (
-    <article className="flex max-w-md flex-col gap-2 rounded-lg bg-navy-darker shadow-md lg:transition-transform lg:duration-500 lg:will-change-transform lg:hover:-translate-y-1 lg:hover:shadow-lg lg:hover:transition-transform lg:hover:duration-150">
-      <ProjectImage src={img.src} alt={img.alt} />
-      <ProjectHeader title={header.title} description={header.description} />
-      <BadgeGroup techStack={techStack} />
-      <ProjectLinks
-        demoURL={links.demo}
-        repoURL={links.repo}
-        localeData={localeData}
-      />
+    <article className="flex min-h-full max-w-md flex-col justify-between gap-2 rounded-lg bg-navy-darker shadow-md lg:transition-transform lg:duration-500 lg:will-change-transform lg:hover:-translate-y-1 lg:hover:shadow-lg lg:hover:transition-transform lg:hover:duration-150">
+      <section className="space-y-2">
+        <ProjectImage src={img.src} alt={img.alt} />
+        <ProjectHeader title={header.title} description={header.description} />
+      </section>
+      <section className="space-y-2">
+        <BadgeGroup techStack={techStack} />
+        <ProjectLinks
+          demoURL={links.demo}
+          repoURL={links.repo}
+          localeData={localeData}
+        />
+      </section>
     </article>
   );
 }
