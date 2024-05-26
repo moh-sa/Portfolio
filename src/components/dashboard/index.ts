aedb31cd9434f03d3bb5d345dcac83c27b27f935
moh-sa <tno.rappelz@gmail.com>
2024-05-26 22:12:27 +0300

    export all dashboard files

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,6 @@
+export * from "./ActionButton";
+export * from "./BackToDashboardLink";
+export * from "./InputField";
+export * from "./ProjectForm";
+export * from "./TextAreaField";
+export * from "./label";
