49afe07f6559d425ccc7048f63955adb666bfbbf
moh-sa <tno.rappelz@gmail.com>
2024-05-26 22:11:37 +0300

    added "profileFields"

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,4 @@
 export * from "./keywords";
 export * from "./profileSkills";
+export * from "./projectFields";
 export * from "./socialLinks";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,2 @@
-export * from "./createProject";
 export * from "./locales";
+export * from "./projectFields";
