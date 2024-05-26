9926ce143892986866d7e715bf29af1356c63ee7
moh-sa <tno.rappelz@gmail.com>
2024-05-27 02:02:58 +0300

    fixed a layout bug in rtl

--- src/components/Project/ProjectCard/ProjectHeader.tsx
+++ src/components/Project/ProjectCard/ProjectHeader.tsx
@@ -5,10 +5,10 @@ type TProps = {
 export function ProjectHeader({ title, description }: TProps) {
   return (
     <hgroup className="mb-1 space-y-1 px-4">
-      <h3 className="border-b border-[#494979] text-4xl font-medium capitalize text-stone-300">
+      <h3 className="border-b border-[#494979] text-4xl font-medium capitalize text-stone-300 rtl:pb-4">
         {title}
       </h3>
-      <p className="text-navy-light max-w-prose hyphens-auto text-justify text-xl">
+      <p className="max-w-prose hyphens-auto text-justify text-xl text-navy-light">
         {description}
       </p>
     </hgroup>
