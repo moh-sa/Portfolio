0b18c8cdd0a9d17019a6dd48ef41bd2ab60fbb92
moh-sa <tno.rappelz@gmail.com>
2024-05-26 21:59:17 +0300

    simplified the design + changed "profile" to "primary" + added "as" prop + changed the hover color

--- src/components/UI/LinkButton.tsx
+++ src/components/UI/LinkButton.tsx
@@ -1,28 +1,22 @@
-type TProps = {
-  url: string;
-  profile?: boolean;
-  children: React.ReactNode;
-  disabled?: boolean;
-};
-export function LinkButton({ url, profile, children, disabled }: TProps) {
-  // 🚫 empty state 🚫
-  if (disabled) {
-    return (
-      <div className="flex flex-1 items-center justify-center gap-2 rounded bg-obsidian-800 py-1 text-xl capitalize text-stone-300 transition-colors duration-500 ease-in hover:bg-obsidian-900 hover:duration-150 hover:ease-out">
-        {children}
-      </div>
-    );
-  }
+import type Link from "next/link";
 
+type LinkProps = {
+  as: "a" | "div" | typeof Link;
+  children: React.ReactNode;
+  url?: string;
+  primary?: boolean;
+};
+export function LinkButton({ as: Element, children, url, primary }: LinkProps) {
   return (
-    <a
-      href={url}
-      target="_blank"
-      className={`flex flex-1 items-center justify-center gap-2 py-1 text-xl capitalize transition-colors duration-500 ease-in hover:duration-150 hover:ease-out
-      ${profile && "rounded bg-pink-200 text-indigo-950 hover:bg-pink-100"}
-      ${!profile && "rounded bg-obsidian-800 text-stone-300 hover:bg-obsidian-900"}`}
+    <Element
+      className={`flex w-full flex-1 items-center justify-center gap-2 rounded px-2 py-1 text-xl capitalize transition-colors duration-500 ease-in hover:duration-150 hover:ease-out
+  ${primary && "bg-pink-200 text-indigo-950 hover:bg-pink-100"}
+  ${!primary && "bg-obsidian-800 text-stone-300 hover:bg-obsidian-500"}
+  `}
+      href={url ?? ""}
+      target={Element === "a" ? "_blank" : "_self"}
     >
       {children}
-    </a>
+    </Element>
   );
 }
