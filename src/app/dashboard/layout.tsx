771918304e7230214d02d47b970e4fb254d5538a
moh-sa <tno.rappelz@gmail.com>
2024-05-27 03:49:22 +0300

    added clerk provider and user button

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,25 +1,30 @@
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
 import { Cairo } from "next/font/google";
 const cairo = Cairo({
   subsets: ["arabic", "latin"],
   variable: "--font-sans",
 });
-
 export const metadata = {
   title: "Dashboard",
 };
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${cairo.variable}`}>
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-navy to-navy-dark py-8">
-          {children}
-        </main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${cairo.variable}`}>
+          <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-navy to-navy-dark py-8">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
