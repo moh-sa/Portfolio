a0249674fd3838cd06f2339c8fecf7abcc896c52
moh-sa <tno.rappelz@gmail.com>
2024-05-29 21:26:43 +0300

    variants for links and buttons

---
+++ src/styles/variants/LinkNButton.ts
@@ -0,0 +1,29 @@
+import { cva } from "class-variance-authority";
+
+export const LinkNButtonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded capitalize transition-colors duration-500 ease-in hover:duration-150 hover:ease-out",
+  {
+    variants: {
+      variant: {
+        primary: "bg-pink-200 hover:bg-pink-100 text-indigo-950 ",
+        secondary: " bg-navy-700 hover:bg-navy-500 text-stone-300",
+        destructive: "bg-red-400 text-white hover:bg-red-300",
+        ghost: "hover:bg-navy-700 bg-transparent text-stone-300",
+      },
+      width: {
+        full: "w-full",
+        auto: "w-auto",
+      },
+      size: {
+        default: "h-9 px-4 py-2 text-lg",
+        sm: "h-8 px-3 text-xs",
+        lg: "h-10 px-8 text-xl",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "default",
+      width: "full",
+    },
+  },
+);
