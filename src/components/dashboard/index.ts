24ccb68e7f85f2e322a74af83cd9df5fff6c5307
moh-sa <tno.rappelz@gmail.com>
2024-05-31 21:50:33 +0300

    remove exports for files relocated to "UI" folder

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,6 +1,2 @@
-export * from "./ActionButton";
 export * from "./BackToDashboardLink";
-export * from "./InputField";
-export * from "./ProjectForm";
-export * from "./TextAreaField";
-export * from "./label";
+export * from "./form";
