93f89072027a9993dd9cdb6a889253637091cf89
moh-sa <tno.rappelz@gmail.com>
2024-06-02 20:48:23 +0300

    simplified the component

--- src/components/dashboard/form/Form.tsx
+++ src/components/dashboard/form/Form.tsx
@@ -1,51 +1,119 @@
-import { Button, Checkbox, InputField, TextAreaField } from "~/components";
-import { groupedFields } from "~/data";
+import {
+  Button,
+  Checkbox,
+  Fieldset,
+  InputField,
+  TextAreaField,
+} from "~/components";
 import { type projectsSchema } from "~/server/db/schema";
 
 type TProps = {
   project?: typeof projectsSchema.$inferSelect;
   formAction: (formData: FormData) => void;
 };
-
 export function Form({ project, formAction }: TProps) {
   return (
     <form className="space-y-6" action={formAction}>
-      {Object.entries(groupedFields).map(([title, group], index) => (
-        <div key={`${index}-${title}`}>
-          <h2 className="text-2xl font-thin capitalize">{title}</h2>
-          <div className="flex flex-col flex-wrap gap-2 lg:flex-row">
-            {group.map((field, index) => {
-              let value = "";
+      {/* ✨ TITLE ✨ */}
+      <Fieldset legend="Title">
+        <InputField
+          required
+          type="text"
+          id="titleEN"
+          label="Title"
+          placeholder="Project Name in English"
+          value={project?.titleEN}
+        />
 
-              if (project) {
-                const key = field.id; // @ts-expect-error I know it's wrong, but I couldn't find a better way to do it :(
-                // eslint-disable-next-line
-                value = project[key];
-              }
+        <InputField
+          isArabic
+          required
+          type="text"
+          id="titleAR"
+          label="اسم العمل"
+          placeholder="اسم العمل بالعربية"
+          value={project?.titleAR}
+        />
+      </Fieldset>
 
-              if (field.as === "input") {
-                return (
-                  <InputField
-                    key={`${index}-${field.id}`}
-                    value={value}
-                    {...field}
-                  />
-                );
-              } else if (field.as === "textarea") {
-                return (
-                  <TextAreaField
-                    key={`${index}-${field.id}`}
-                    value={value}
-                    {...field}
-                  />
-                );
-              } else if (field.as === "checkbox") {
-                return <Checkbox key={`${index}-${field.id}`} {...field} />;
-              }
-            })}
-          </div>
-        </div>
-      ))}
+      {/* ✨ DESCRIPTION ✨ */}
+      <Fieldset legend="Description">
+        <TextAreaField
+          id="descriptionEN"
+          label="Description"
+          placeholder="Project Description in English"
+          value={project?.descriptionEN}
+        />
+
+        <TextAreaField
+          isArabic
+          id="descriptionAR"
+          label="الوصف"
+          placeholder="وصف العمل بالعربية"
+          value={project?.descriptionAR}
+        />
+      </Fieldset>
+
+      {/* ✨ IMAGE ✨ */}
+      <Fieldset legend="Image">
+        <InputField type="file" id="imageURL" label="Image Upload" />
+
+        <TextAreaField
+          id="imageAltEN"
+          label="Image Alt Text"
+          placeholder="Image Alt in English"
+          value={project?.imageAltEN}
+        />
+
+        <TextAreaField
+          isArabic
+          id="imageAltAR"
+          label="وصف الصورة"
+          placeholder="وصف الصورة بالعربية"
+          value={project?.imageAltAR}
+        />
+      </Fieldset>
+
+      {/* ✨ LINKS✨ */}
+      <Fieldset legend="Links">
+        <InputField
+          type="url"
+          id="demoURL"
+          label="Demo URL"
+          placeholder="Live demo URL"
+          value={project?.demoURL}
+        />
+
+        <InputField
+          type="url"
+          id="repoURL"
+          label="Repo URL"
+          placeholder="Github repo URL"
+          value={project?.repoURL}
+        />
+      </Fieldset>
+
+      {/* ✨ TECH STACK ✨ */}
+      <Fieldset legend="Tech Stack">
+        <InputField
+          type="text"
+          id="techStack"
+          label="Tech Stack"
+          placeholder="Tech,AnotherTech,OneMoreTech"
+          value={project?.techStack}
+        />
+      </Fieldset>
+
+      {/* ✨ Checkboxes ✨ */}
+      <Fieldset legend="Other">
+        <Checkbox id="hidden" label="Hidden" defaultChecked={project?.hidden} />
+        <Checkbox
+          id="isOriginal"
+          label="Is Original"
+          defaultChecked={project?.isOriginal}
+        />
+      </Fieldset>
+
       <Button type="submit">Submit</Button>
     </form>
   );
