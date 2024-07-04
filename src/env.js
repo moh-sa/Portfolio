0791c5ec5f39206018e50f7f1b81f1d7d08da595
moh-sa <tno.rappelz@gmail.com>
2024-07-04 21:55:29 +0300

    simplified the env file

--- src/env.js
+++ src/env.js
@@ -1,58 +1,44 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { z, ZodError } from "zod";
 
-export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
-  server: {
-    DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(["development", "preview", "production"])
-      .default("development"),
-    CLOUDINARY_API_SECRET: z.string(),
-  },
+const EnvSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "preview", "production"])
+    .default("development"),
 
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
-  client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_URL: z.string(),
-    NEXT_PUBLIC_CLOUDINARY_API_KEY: z.string(),
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string(),
-  },
+  NEXT_PUBLIC_URL: z.string().url(),
 
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_URL:
-      process.env.NODE_ENV === "development" ||
-      process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-        ? process.env.NEXT_PUBLIC_URL
-        : "https://" + process.env.NEXT_PUBLIC_VERCEL_URL,
+  NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string(),
+  NEXT_PUBLIC_CLOUDINARY_API_KEY: z.string(),
+  CLOUDINARY_API_SECRET: z.string(),
 
-    // CLOUDINARY
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
-      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    NEXT_PUBLIC_CLOUDINARY_API_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
-  },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
-  emptyStringAsUndefined: true,
+  DB_HOST: z.string(),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_NAME: z.string(),
+  DB_PORT: z.string(),
+  DATABASE_URL: z.string().url(),
 });
+
+expand(config());
+
+try {
+  EnvSchema.parse(process.env);
+} catch (error) {
+  if (error instanceof ZodError) {
+    let message = "❌ Missing required values in .env:\n";
+    error.issues.forEach((issue) => {
+      message += issue.path[0] + "\n";
+    });
+
+    const e = new Error(message);
+    e.stack = "";
+
+    throw e;
+  } else {
+    console.error(error);
+  }
+}
+
+export const env = EnvSchema.parse(process.env);
