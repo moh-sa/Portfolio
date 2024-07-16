33272ac7a6305295d896f826c117943ddd85f75b
moh-sa <tno.rappelz@gmail.com>
2024-07-16 18:55:07 +0300

    refactor to accept 'children' prop instead of hardcoded values

--- src/components/dashboard/form/FormElement.tsx
+++ src/components/dashboard/form/FormElement.tsx
@@ -1,117 +1,13 @@
-import { Checkbox, Fieldset, InputField, TextAreaField } from "~/components";
 import { FormStatusButtonWrapper } from "~/components/UI/Buttons/ClientFormButtonWrapper";
-import { type insertProjectSchema } from "~/server/db/schemas";
 
 type TProps = {
-  project?: typeof insertProjectSchema._type;
   formAction: (formData: FormData) => void;
+  children: React.ReactNode;
 };
-export function FormElement({ project, formAction }: TProps) {
+export function FormElement({ formAction, children }: TProps) {
   return (
     <form className="space-y-6" action={formAction}>
-      {/* ✨ TITLE ✨ */}
-      <Fieldset legend="Title">
-        <InputField
-          required
-          type="text"
-          id="titleEN"
-          label="Title"
-          placeholder="Project Name in English"
-          value={project?.titleEN}
-        />
-
-        <InputField
-          isArabic
-          required
-          type="text"
-          id="titleAR"
-          label="اسم العمل"
-          placeholder="اسم العمل بالعربية"
-          value={project?.titleAR}
-        />
-      </Fieldset>
-
-      {/* ✨ DESCRIPTION ✨ */}
-      <Fieldset legend="Description">
-        <TextAreaField
-          id="descriptionEN"
-          label="Description"
-          placeholder="Project Description in English"
-          value={project?.descriptionEN}
-        />
-
-        <TextAreaField
-          isArabic
-          id="descriptionAR"
-          label="الوصف"
-          placeholder="وصف العمل بالعربية"
-          value={project?.descriptionAR}
-        />
-      </Fieldset>
-
-      {/* ✨ IMAGE ✨ */}
-      <Fieldset legend="Image">
-        <InputField type="file" id="imageURL" label="Image Upload" />
-
-        <TextAreaField
-          id="imageAltEN"
-          label="Image Alt Text"
-          placeholder="Image Alt in English"
-          value={project?.imageAltEN}
-        />
-
-        <TextAreaField
-          isArabic
-          id="imageAltAR"
-          label="وصف الصورة"
-          placeholder="وصف الصورة بالعربية"
-          value={project?.imageAltAR}
-        />
-      </Fieldset>
-
-      {/* ✨ LINKS✨ */}
-      <Fieldset legend="Links">
-        <InputField
-          type="url"
-          id="demoURL"
-          label="Demo URL"
-          placeholder="Live demo URL"
-          value={project?.demoURL}
-        />
-
-        <InputField
-          type="url"
-          id="repoURL"
-          label="Repo URL"
-          placeholder="Github repo URL"
-          value={project?.repoURL}
-        />
-      </Fieldset>
-
-      {/* ✨ TECH STACK ✨ */}
-      <Fieldset legend="Tech Stack">
-        <InputField
-          type="text"
-          id="techStack"
-          label="Tech Stack"
-          placeholder="Tech,AnotherTech,OneMoreTech"
-          value={project?.techStack}
-        />
-      </Fieldset>
-
-      {/* ✨ Checkboxes ✨ */}
-      <Fieldset legend="Other">
-        <Checkbox
-          id="isHidden"
-          label="Is Hidden"
-          defaultChecked={project?.isHidden}
-        />
-        <Checkbox
-          id="isOriginal"
-          label="Is Original"
-          defaultChecked={project?.isOriginal}
-        />
-      </Fieldset>
+      {children}
       <FormStatusButtonWrapper>Submit</FormStatusButtonWrapper>
     </form>
   );
--- src/components/dashboard/form/FormWrapper.tsx
+++ src/components/dashboard/form/FormWrapper.tsx
@@ -1,22 +1,18 @@
-import { type selectProjectSchema } from "~/server/db/schemas";
-
 import { FormHeader } from "./FormHeader";
 
 import { FormElement } from "./FormElement";
 
 type TProps = {
   heading: string;
-
-  project?: typeof selectProjectSchema._type;
-
   formAction: (formData: FormData) => void;
+  children: React.ReactNode;
 };
 
-export function FormWrapper({ heading, project, formAction }: TProps) {
+export function FormWrapper({ heading, formAction, children }: TProps) {
   return (
     <div className="w-full max-w-xl px-4">
       <FormHeader heading={heading} />
-      <FormElement project={project} formAction={formAction} />
+      <FormElement formAction={formAction}>{children}</FormElement>
     </div>
   );
 }
